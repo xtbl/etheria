7b52cbb737024023fcc05c1cc549f54831f89ddf
xtbl <correodecristobal@gmail.com>
2014-03-20 15:07:09 -0600

    add: main page video, masonry working

--- public/js/products/ProductListCtrl.js
+++ public/js/products/ProductListCtrl.js
@@ -3,4 +3,11 @@
 angular.module('etheria.products').controller('ProductListCtrl', ['$scope', 'CachedProduct', function ($scope, CachedProduct) {
     $scope.productImagePath = '/img/products/';
     $scope.products = CachedProduct.query();
-}]);
+}])
+    .directive('testDirective', function() {
+        return {
+            template: '<h1>testDirective</h1>'
+        }
+    })
+
+;
